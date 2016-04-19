9be4ce368e986fd975d21368ed15d1f37e75b303
lukasz.ksiezak <lukasz.ksiezak@interia.pl>
2016-04-19 22:19:22 +0200

    Working beta application

---
+++ StateControlTests/SiteReaderTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StateControl;
+using StateControl.Models;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace StateControlTests
+{
+    [TestClass]
+    public class SiteReaderTest
+    {
+        [TestMethod]
+        public void StateControl_ShouldReturnOnline()
+        {
+            string address = "www.google.pl";
+            Site sampleWebsite = new Site("1", address);
+
+            Assert.IsNotNull(sampleWebsite.SiteAddress);
+            Assert.AreEqual<ServerStatus>(sampleWebsite.SiteStatus, ServerStatus.Online);
+        }
+
+        [TestMethod]
+        public void StateControl_ShouldReturnOffline()
+        {
+            string address = "192.168.10.10";
+            Site sampleWebsite = new Site("1", address);
+
+            Assert.IsNotNull(sampleWebsite.SiteAddress);
+            Assert.AreEqual<ServerStatus>(sampleWebsite.SiteStatus, ServerStatus.Offline);
+        }
+    }
+}
